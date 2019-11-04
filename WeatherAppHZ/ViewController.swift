10c824317560f630f62af4ec4045489fe4c228d2
hzamorano <hzamorano@nearsoft.com>
2019-11-04 13:22:29 -0700

    Adds Alamofire and SwiftyJSON lib

--- WeatherAppHZ/ViewController.swift
+++ WeatherAppHZ/ViewController.swift
@@ -6,15 +6,59 @@
 //  Copyright © 2019 Hiram Zamorano. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Alamofire
+import SwiftyJSON
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var tempMinLabel: UILabel!
+    @IBOutlet weak var tempMaxLabel: UILabel!
+    
+    let apiKey = "c70ff4cc260c1075bbfb6849caa1ad29"
+    var lat = 11.34
+    var lon = 104.33
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
 
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                //let jsonweather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                self.tempMinLabel.text = "\(Int(round(jsonTemp["temp_min"].doubleValue)))"
+                self.tempMaxLabel.text = "\(Int(round(jsonTemp["temp_max"].doubleValue)))"
+            }
+        }
+        
+    }
 
+    
 }
 
