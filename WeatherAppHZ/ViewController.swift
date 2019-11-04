4e451096fda41a777653609cc4a3914621773a97
hzamorano <hzamorano@nearsoft.com>
2019-11-04 11:51:04 -0700

    Adds main storyboard

---
+++ WeatherAppHZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppHZ
+//
+//  Created by Hiram Zamorano on 11/4/19.
+//  Copyright © 2019 Hiram Zamorano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
